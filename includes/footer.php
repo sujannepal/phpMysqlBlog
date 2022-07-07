8ed3d3d5058660524a31414f76bbfa32b5aff99d
sujannepal <sujannepal89@gmail.com>
2022-07-07 18:43:05 +0545

    minor

--- includes/footer.php
+++ includes/footer.php
@@ -1,9 +1,9 @@
 
 </div>
-<footer class="fixed-bottom bg-dark text-white">
+<!-- <footer class="fixed-bottom bg-dark text-white">
     <p class="text-center p-4 m-0">&copy; Fog Computing 2022</p>
 
-</footer>
+</footer> -->
 </div>
 
 
